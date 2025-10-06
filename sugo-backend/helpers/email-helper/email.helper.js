6d78c13f601b289dd43f0af630dee69f51dabc7c
zain100000 <muhammadzainulabideen292@gmail.com>
2025-10-06 18:53:08 +0500

    modify helper function

--- sugo-backend/helpers/email-helper/email.helper.js
+++ sugo-backend/helpers/email-helper/email.helper.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 25,
+  port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
